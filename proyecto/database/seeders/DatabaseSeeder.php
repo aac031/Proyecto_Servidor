79dfacf7f30ca20753a7b3275a3f5a8c7c830795
aac031 <113922069+aac031@users.noreply.github.com>
2023-02-18 22:20:10 +0100

    v1.5

--- proyecto/database/seeders/DatabaseSeeder.php
+++ proyecto/database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AdministratorsSeeder::class,
             TreatmentSeeder::class,
+            SocioSeeder::class,
         ]);
     }
 }
